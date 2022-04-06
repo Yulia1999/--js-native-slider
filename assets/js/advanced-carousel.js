020c39085f61202de32e03a73f3cea54d7cb097c
Yulia1999 <y.kotz@mail.ru>
2022-04-06 21:26:15 +0300

    first commit

--- assets/js/advanced-carousel.js
+++ assets/js/advanced-carousel.js
@@ -1,19 +1,17 @@
-import Carousel from './basic-carousel.js';
-export default class SwipeCarousel extends Carousel{
-	
-initListeners () {
-	super.initListeners()
-	this.container.addEventListener("touchStart", this.swipeStart.bind(this));
-	this.container.addEventListener("touchEnd", this.swipeEnd.bind(this));
-	}
-	
-swipeStart (e) {
-	  this.swipeStartX = e.changedTouchers[0].pageX;
-	}
-swipeEnd (e) {
-	  this.swipeEndX = e.changedTouchers[0].pageX;
-	  this.swipeStartX - swipeEndX < -100 && this.prev();
-	  this.swipeStartX - swipeEndX > 100 && this.next();
-	}
-}
+import Carousel from "./basic-carousel.js";
+export default class SwipeCarousel extends Carousel {
+  initListeners() {
+    super.initListeners();
+    this.container.addEventListener("touchStart", this.swipeStart.bind(this));
+    this.container.addEventListener("touchEnd", this.swipeEnd.bind(this));
+  }
 
+  swipeStart(e) {
+    this.swipeStartX = e.changedTouchers[0].pageX;
+  }
+  swipeEnd(e) {
+    this.swipeEndX = e.changedTouchers[0].pageX;
+    this.swipeStartX - swipeEndX < -100 && this.prev();
+    this.swipeStartX - swipeEndX > 100 && this.next();
+  }
+}
